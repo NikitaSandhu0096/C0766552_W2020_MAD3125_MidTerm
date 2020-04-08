8f35bad5324fed067fdc31edc5828e9260c5e1ec
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-08 15:14:27 -0400

    [Nikita] - customer details activity

--- app/src/main/java/com/example/c0766552_w2020_mad3125_midterm/ui/MainActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_midterm/ui/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
