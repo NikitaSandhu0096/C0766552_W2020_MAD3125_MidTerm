95d60b0d1e3453031eaeb710e4838d682ff60dc9
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-08 00:07:10 -0400

    [Nikita] - PIN NAME id in main

--- app/src/main/java/com/example/c0766552_w2020_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_midterm/MainActivity.java
@@ -8,17 +8,17 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private TextInputLayout txtPIN;
-//    private TextInputLayout txtfName;
-//    private TextInputLayout txtlName;
+    private TextInputLayout txtPIN;
+    private TextInputLayout txtfName;
+    private TextInputLayout txtlName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        txtPIN = findViewById(R.id.personSINNumberTextInputLayout);
-//        txtfName= findViewById(R.id.firstNameTextInputLayout);
-//        txtlName = findViewById(R.id.lastNameTextInputLayout);
+        txtPIN = findViewById(R.id.personSINNumberTextInputLayout);
+        txtfName= findViewById(R.id.firstNameTextInputLayout);
+        txtlName = findViewById(R.id.lastNameTextInputLayout);
     }
 }
