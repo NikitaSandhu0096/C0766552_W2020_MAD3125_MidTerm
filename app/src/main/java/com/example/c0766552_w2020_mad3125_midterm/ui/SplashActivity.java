6170f7b82285427edaac5ce29561dadf94f9eede
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-04-07 21:27:28 -0400

    [Nikita] Splash Screen

---
+++ app/src/main/java/com/example/c0766552_w2020_mad3125_midterm/ui/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.c0766552_w2020_mad3125_midterm.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.c0766552_w2020_mad3125_midterm.MainActivity;
+import com.example.c0766552_w2020_mad3125_midterm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent hint = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(hint);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
